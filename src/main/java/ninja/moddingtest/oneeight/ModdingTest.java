90a515d119ec51d55354795b4b407ac7e60dd707
violentninjad <fiddlybilly@gmail.com>
2014-11-29 10:05:46 +0000

    First steps and stuff :D

---
+++ src/main/java/ninja/moddingtest/oneeight/ModdingTest.java
@@ -0,0 +1,15 @@
+package ninja.moddingtest.oneeight;
+
+import net.minecraftforge.fml.common.Mod;
+
+/** @author ViolentNinjaD
+    LGPLv3
+**/
+
+@Mod(modid = ModdingTest.MOD_ID, name = ModdingTest.MOD_NAME, version = ModdingTest.MOD_VERSION)
+public class ModdingTest 
+{
+    public static final String MOD_ID = "moddingtest";
+    public static final String MOD_NAME = "ModdingTest";
+    public static final String MOD_VERSION = "@VERSION@";
+}
